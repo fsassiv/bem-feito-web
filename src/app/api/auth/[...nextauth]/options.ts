ac969b23d5657ff7ac37dc37533325e04b11410b
Flavio Andrade <flavio.ssiv@gmail.com>
2024-07-02 14:30:11 -0300

    feat: Initial set up next-auth, next-intl, shadcn

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,33 @@
+import type { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const NEXT_AUTH_OPTIONS: NextAuthOptions = {
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Username",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { id: "001", name: "_teste", password: "teste@123" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  // secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/sigin",
+  },
+};
