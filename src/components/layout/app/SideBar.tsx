9a27fbc3c63b5ec56cfcca8a15bd191a864de78a
Flavio Andrade <flavio.ssiv@gmail.com>
2024-07-07 00:09:19 -0300

    ...

--- src/components/layout/app/SideBar.tsx
+++ src/components/layout/app/SideBar.tsx
@@ -12,7 +12,7 @@ export const SideBar = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.5 }}
-      className="w-[20%] h-full max-lg:hidden pl-2 pt-2"
+      className="w-[20%] 2xl:w-[15%] h-full max-lg:hidden pl-2 pt-2"
     >
       <FilterBar />
     </motion.aside>
