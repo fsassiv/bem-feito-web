712b611576cd97fb4a1af1e04b676266868212b2
Flavio Andrade <flavio.ssiv@gmail.com>
2024-07-08 19:30:21 -0300

    test

--- src/context/session/index.tsx
+++ src/context/session/index.tsx
@@ -2,6 +2,8 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
+console.log("first");
+
 export const SessionWrapper = ({ children }: { children: ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
